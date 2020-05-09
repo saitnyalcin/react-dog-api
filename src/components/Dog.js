dd0693b5efc5028d250490189834959574f6f82b
steveyalcin <steven.yalcin@gmail.com>
2020-05-08 22:38:54 -0300

    Dog details are seperated and moved into modular component

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import DogDetails from './DogDetails';
 
 export default function Dog() {
   const [data, setData] = useState([]);
@@ -14,18 +15,22 @@ export default function Dog() {
     <div>
       {data.map(dog => (
         <div key={dog.id}>
-          <img src={dog.url} alt="" height="400px" width="400px" />
-
-          <div>
-            {dog.breeds.map(details => (
-              <div>
-                <p>{details.name}</p>
-                <p>{details.bred_for}</p>
-                <p>{details.breed_group}</p>
-                <p>{details.life_span}</p>
-                <p>{details.temperament}</p>
-              </div>
-            ))}
+          <div className="main-flex-container">
+            <div>
+              {dog.breeds.map(details => (
+                <DogDetails
+                  key={details.id}
+                  name={details.name}
+                  bred_for={details.bred_for}
+                  breed_group={details.breed_group}
+                  life_span={details.life_span}
+                  temperament={details.temperament}
+                />
+              ))}
+            </div>
+            <div>
+              <img src={dog.url} alt="" height="400px" width="400px" />
+            </div>
           </div>
         </div>
       ))}
